e7d267f89a81327460b99da631beb837035a36bc
Darkv1c <vfearpro@gmail.com>
2021-05-10 09:01:27 -0500

    style
    added style to footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Navbar, Footer} from './components'
+import {Home} from './pages'
 
 const App = () => {
     return (
         <div className="App">
             <Navbar/>
+            <Home/>
             <Footer/>
         </div>
     )
