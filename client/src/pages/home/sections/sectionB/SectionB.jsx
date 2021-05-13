84a6f5fc82c502c4b9138811cced957e1e10b303
Darkv1c <vfearpro@gmail.com>
2021-05-13 00:41:56 -0500

    fixed background

---
+++ client/src/pages/home/sections/sectionB/SectionB.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Style.scss'
+
+const SectionB = () => {
+    return (
+        <div className="SectionB">
+            
+        </div>
+    )
+}
+
+export default SectionB
