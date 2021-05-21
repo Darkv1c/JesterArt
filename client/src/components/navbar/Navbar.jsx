7cc88aca5e455a3af1cbf0214c8bdfa8808d9d50
Darkv1c <vfearpro@gmail.com>
2021-05-20 22:30:59 -0500

    Section A

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './Style.css'
 import {Logo, Menu, Close} from '../../assets/icons'
 import { Link } from 'react-router-dom'
